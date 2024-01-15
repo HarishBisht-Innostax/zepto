07733e624af5b5ef10b4fd11aa986bffbc59d04c
Harish Bisht <harish.bisht@innostax.com>
2024-01-16 02:11:46 +0530

    MultiSelect Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AutoSelect from "./AutoSelect";
+import { USERS } from "./mockdata";
 
 function App() {
+  const handleSelectChange = (selectedOption) => {
+    console.log("selectedOption", selectedOption);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex justify-center items-center mt-16 w-full">
+      <div>
+        <AutoSelect options={USERS} onChange={handleSelectChange} />
+      </div>
     </div>
   );
 }
